c4b5b2202c5cf4ee4e7a46cea87d7b7d453cea56
MingLongSu <bananafrenzy31@gmail.com>
2022-06-06 23:01:03 -0400

    Calculates changes to weights Updated shape of train labels

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from nn import NeuralNetwork
+from dnn import NeuralNetwork
 from fetch_data import fetch_mnist
 import numpy as np
 
@@ -6,9 +6,12 @@ import numpy as np
 
 (images, labels), (images_2, labels_2) = fetch_mnist()
 img = images[0]
+lbl = labels[0]
 
 nn = NeuralNetwork()
 
 results = nn.forward_propogate(img)
-print(np.argmax(results))
-print(results)
+update = nn.backward_propogation(results, lbl, 0.01, img)
+print(update['update_w_h2_o'].shape)
+print(update['update_w_h1_h2'].shape)
+print(update['update_w_i_h1'].shape)
